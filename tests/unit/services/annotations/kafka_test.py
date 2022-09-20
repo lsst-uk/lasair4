fcd9ee875560d9fecc8721f15fff373a16915b71
G Francis <g.francis@epcc.ed.ac.uk>
2022-09-20 13:41:16 +0100

    move annotations tests

--- tests/unit/services/annotations/kafka_test.py
+++ tests/unit/services/annotations/kafka_test.py
@@ -1,6 +1,6 @@
 import os, sys, json
 import unittest
-python_path = '../../services/annotations/kafkaPush'
+python_path = '../../../../services/annotations/kafkaPush'
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), python_path)))
 from kafka2annotation import process_annotations
 
@@ -19,4 +19,6 @@ class TestAnnotations(unittest.TestCase):
             self.assertEqual(queries_from_code[i], queries_from_file[i])
 
 if __name__ == '__main__':
-    unittest.main()
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
