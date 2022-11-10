f5db69711372747858b411ba32aa051ef97b0e23
G Francis <g.francis@epcc.ed.ac.uk>
2022-11-10 12:04:33 +0000

    initial implementation

---
+++ common/src/lasairLogging.py
@@ -0,0 +1,16 @@
+"""Common Lasair logging module. Based on standard logging module with sensible default
+values for Lasair. Sets up a log file and also sends error and above messages to Slack."""
+
+import logging
+
+
+def basicConfig(filename, slack_webhook=None, level=logging.INFO):
+    logging.basicConfig(filename=filename, level=level)
+
+
+def getLogger(name):
+    return logging.getLogger(name)
+
+
+def shutdown():
+    logging.shutdown()
