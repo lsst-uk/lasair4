a894e3af0e2ec79d57a7173127966ab5ebf8be07
G Francis <g.francis@epcc.ed.ac.uk>
2022-11-10 17:42:51 +0000

    get slack alerts working

--- common/src/lasairLogging.py
+++ common/src/lasairLogging.py
@@ -1,15 +1,36 @@
 """Common Lasair logging module. Based on standard logging module with sensible default
 values for Lasair. Sets up a log file and also sends error and above messages to Slack."""
 
-import logging
+import os, logging
+#import slack_webhook
 
 
-def basicConfig(filename, slack_webhook=None, level=logging.INFO):
+class SlackHandler(logging.Handler):
+    def __init__(self, webhook):
+        super().__init__()
+        self.webhook = webhook
+
+    def emit(self, record):
+        """Emit a record."""
+        msg = self.format(record)
+        self.webhook.send(msg)
+
+
+def basicConfig(filename, webhook=None, level=logging.INFO, force=False):
     logging.basicConfig(
         filename=filename,
         level=level,
-        format="[%(asctime)s]:%(levelname)s:%(funcName)s:%(message)s"
+        format="[%(asctime)s] %(levelname)s: %(funcName)s: %(message)s",
+        force=force
     )
+    if webhook is not None:
+        hostname = os.uname().nodename
+        slack_formatter = logging.Formatter(
+            "%(levelname)s: {}: %(funcName)s: %(message)s".format(hostname))
+        slack_handler = SlackHandler(webhook)
+        slack_handler.setLevel(logging.ERROR)
+        slack_handler.setFormatter(slack_formatter)
+        logging.getLogger().addHandler(slack_handler)
 
 
 def getLogger(name):
--- tests/unit/common/src/test_logging.py
+++ tests/unit/common/src/test_logging.py
@@ -2,6 +2,7 @@
 import context
 import lasairLogging
 import os
+import glob
 import unittest, unittest.mock
 # from manage_status import manage_status
 
@@ -10,29 +11,57 @@ class CommonLoggingTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        if os.path.exists("logging_test.log"):
-            os.remove("logging_test.log")
-        with unittest.mock.MagicMock() as mock_slack_webhook:
-            lasairLogging.basicConfig(
-                filename="logging_test.log",
-                slack_webhook=mock_slack_webhook
-            )
-
+        for name in glob.glob("test_*.log"):
+            if os.path.exists(name):
+                os.remove(name)
 
     @classmethod
     def tearDownClass(cls):
         lasairLogging.shutdown()
-        if os.path.exists("logging_test.log"):
-            os.remove("logging_test.log")
 
     def test_new_logger(self):
         """Start a new logger with a new output file, log a message. The output
         file should exist and contain the message in the right format."""
+        lasairLogging.basicConfig(
+            filename="test_new_logger.log",
+            force=True
+        )
         log = lasairLogging.getLogger("test_logger")
         log.info("Test message")
-        with open("logging_test.log", "r") as f:
+        with open("test_new_logger.log", "r") as f:
             self.assertRegex(f.readline().strip(),
-                             "^\\[.*\\]:INFO:test_new_logger:Test message")
+                             "^\\[.*\\] INFO: test_new_logger: Test message")
+
+    def test_existing_logger(self):
+        """Start a logger with an existing output file, log a message.
+        The output file should contain both messages in the right format."""
+        with open("test_existing_logger.log", "w") as f:
+            f.write("An existing line\n")
+        lasairLogging.basicConfig(
+            filename="test_existing_logger.log",
+            force=True
+        )
+        log = lasairLogging.getLogger("test_logger")
+        log.info("Test message 2")
+        with open("test_existing_logger.log", "r") as f:
+            self.assertRegex(f.readlines()[-1].strip(),
+                             "^\\[.*\\] INFO: test_existing_logger: Test message 2")
+
+    def test_slack_logger(self):
+        """Start a logger configured to send >=ERROR to Slack (mock).
+        Send an error message. The (mock) Slack webhook should be called."""
+        with unittest.mock.MagicMock() as mock_slack_webhook:
+            lasairLogging.basicConfig(
+                filename="test_slack_logger.log",
+                webhook=mock_slack_webhook,
+                force=True
+            )
+            log = lasairLogging.getLogger("test_logger")
+            log.error("Test message 3")
+            with open("test_slack_logger.log", "r") as f:
+                self.assertRegex(f.readlines()[-1].strip(),
+                                 "^\\[.*\\] ERROR: test_slack_logger: Test message 3")
+            mock_slack_webhook.send.assert_called_once()
 
 
 if __name__ == '__main__':
