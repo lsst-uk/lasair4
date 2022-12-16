a65bc36e972e0d5dd439ef2150d2ad760a5ab5ef
Dave <d.r.young@qub.ac.uk>
2022-12-16 13:35:54 +0000

    updated captcha settings

--- webserver/users/forms.py
+++ webserver/users/forms.py
@@ -10,11 +10,10 @@ from django.conf import settings
 
 class UserRegisterForm(UserCreationForm):
     captcha = ReCaptchaField(widget=ReCaptchaV3(attrs={
-        'required_score': 0.0
+        'required_score': 0.85
     }))
     email = forms.EmailField()
     if getattr(settings, "DEBUG", False) and getattr(settings, "LASAIR_URL", False) == "127.0.0.1":
-        print("HERHEHRERHERHERHERHERHERHERHERHERHER")
         captcha.clean = lambda x: True
 
     def __init__(self, *args, **kwargs):
