7e032ceee27901dc95bcc05bc11e8830df0a09bb
Dave <d.r.young@qub.ac.uk>
2022-12-20 12:48:41 +0000

    x

--- webserver/users/urls.py
+++ webserver/users/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html", post_reset_login=True, success_url="/password-reset/success/"), name='password_reset_confirm'),
     path('password-reset/success/', auth_views.PasswordResetDoneView.as_view(template_name="index.html", extra_context={"alert": "You have successfully changed your password and are now logged in."}), name='password_reset_success'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name="users/password_reset.html", email_template_name="users/password_reset_email.html"), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="index.html", extra_context={"alert": "If your email is associated with a registered account, you will will receive an email with instructions on how to change your password."}), name='password_reset_done'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="index.html", extra_context={"alert": "If your email is associated with a registered account, you will receive an email with instructions on how to change your password."}), name='password_reset_done'),
 ]
 
 if settings.DEBUG:
