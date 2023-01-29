1675da09768fb0afbcea157b3bba2af0e9bd9b8e
lasair-dev-web machine <lasair@lasair.com>
2023-01-29 08:40:44 +0000

    first try with celestial

--- webserver/lasair/views.py
+++ webserver/lasair/views.py
@@ -24,8 +24,28 @@ sys.path.append('../common')
 
 
 def index(request):
+    query = """
+       SELECT objects.objectId,
+           objects.gmag, objects.rmag, jdnow()-objects.jdmax AS age,
+           sherlock_classifications.classification AS CLASS
+       FROM objects, sherlock_classifications
+       WHERE objects.objectId=sherlock_classifications.objectId
+           AND objects.jdmax > jdnow()-14
+           AND (objects.gmag < 18 OR objects.rmag < 18)
+           AND objects.ncandgp > 3
+           AND sherlock_classifications.classification in ("AGN", "CV", "NT", "SN")
+    """
+
+    msl = db_connect.readonly()
+    cursor = msl.cursor(buffered=True, dictionary=True)
+    cursor.execute (query)
+    alerts = [alert for alert in cursor]
+    message = str(alerts)[:300]
+    news = open('/home/ubuntu/news.txt').read()
     context = {
         'web_domain': settings.WEB_DOMAIN,
-        'message'   : 'hello'
+        'alerts'    : alerts,
+        'news'      : news,
+        'message'   : message
     }
     return render(request, 'index.html', context)
