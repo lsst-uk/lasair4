34cc921f9d88bf667a59e0ed995e3e64da0c7006
lasair-dev-web machine <lasair@lasair.com>
2023-02-01 12:01:46 +0000

    new sidebar

--- webserver/lasair/apps/annotator/urls.py
+++ webserver/lasair/apps/annotator/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('annotators/', views.annotators, name='annotators'),
+    path('annotator/', views.annotators, name='annotators'),
 ]
