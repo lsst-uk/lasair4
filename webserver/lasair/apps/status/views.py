547084993eb2d448024ed611f371180abf03eee0
Dave <d.r.young@qub.ac.uk>
2023-02-10 16:07:29 +0000

    status page functionality

--- webserver/lasair/apps/status/views.py
+++ webserver/lasair/apps/status/views.py
@@ -50,22 +50,50 @@ def status(request, nid):
         jsonstr = open(filename).read()
     except:
         jsonstr = ''
-        return render(request, 'error.html', {'message': 'Cannot open status file for nid=%d' % nid})
+        # return render(request, 'error.html', {'message': 'Cannot open status file for nid=%d' % nid})
 
     try:
         status = json.loads(jsonstr)
     except:
         status = None
-        return render(request, 'error.html', {'message': 'Cannot parse status file for nid=%d' % nid})
+        # return render(request, 'error.html', {'message': 'Cannot parse status file for nid=%d' % nid})
 
     if status and 'today_filter' in status:
         status['today_singleton'] = \
             status['today_filter'] - status['today_filter_out'] - status['today_filter_ss']
 
+    # KEY: (DEFINITION, COMMENT)
+    statusSchema = {
+        'today_alert': ('Alerts received by Ingest stage today', ''),
+        'today_candidate': ('New detections today', ''),
+        'update_time': ('Last Lasair update time (UTC)', ''),
+        'today_filter': ('Alerts received by Filter stage today', 'Database Alerts + Solar System Alerts'),
+        'today_filter_out': ('Alerts sent to database today', ''),
+        'today_filter_ss': ('Solar system detections today', ''),
+        'today_ztf': ('Alerts sent by ZTF today', ''),
+        'today_database': ('Updated objects in database today', ''),
+        'total_count': ("Total objects in database", ''),
+        'min_delay': ('Since most recent alert, hours:minutes', ''),
+        'nid': ('Night number (nid)', ''),
+        'countTNS': ('Number in TNS database', ''),
+        'today_singleton': ('Singletons today', '')
+    }
+    statusOrder = ["total_count", "nid", "update_time", "today_ztf", "today_alert", "today_filter_out", "today_filter_ss", "today_filter", "today_singleton", "today_candidate", "today_database", "min_delay", "countTNS"]
+
+    statusTable = []
+
+    if status:
+        statusTable[:] = [(statusSchema[s][0], status[s], statusSchema[s][1]) for s in statusOrder]
+
     date = date_nid.nid_to_date(nid)
+    prettyDate = date_nid.nid_to_pretty_date(nid)
+    daysAgo = date_nid.nid_to_days_ago(nid)
     return render(request, 'status.html', {
-        'web_domain': web_domain, 
-        'status': status, 
-        'date': date, 
+        'web_domain': web_domain,
+        'status': statusTable,
+        'date': date,
+        'daysAgo': daysAgo,
+        'nid': nid,
+        'prettyDate': prettyDate,
         'lasair_grafana_url': settings.LASAIR_GRAFANA_URL,
         'message': message})
--- webserver/lasair/views.py
+++ webserver/lasair/views.py
@@ -28,37 +28,3 @@ def index(request):
         'web_domain': settings.WEB_DOMAIN
     }
     return render(request, 'index.html', context)
-
-
-def status_today(request):
-    nid  = date_nid.nid_now()
-    return status(request, nid)
-
-def status(request, nid):
-    message = ''
-    web_domain = settings.WEB_DOMAIN
-    try:
-        filename = '%s_%d.json' % (settings.SYSTEM_STATUS, nid)
-        jsonstr = open(filename).read()
-    except:
-        jsonstr = ''
-        return render(request, 'error.html', {'message': 'Cannot open status file for nid=%d'%nid})
-
-    if 1:
-#    try:
-        status = json.loads(jsonstr)
-        return(render(request, 'error.html', {'message': str(status)}))
-
-
-#    except:
-#        status = None
-#        return render(request, 'error.html', {'message': 'Cannot parse status file for nid=%d'%nid})
-
-    if status and 'today_filter' in status:
-        status['today_singleton'] = \
-            status['today_filter'] - status['today_filter_out'] - status['today_filter_ss']
-
-    date = date_nid.nid_to_date(nid)
-    return render(request, 'status.html', 
-            {'web_domain': web_domain, 'status':status, 'date':date, 'message':message})
-
