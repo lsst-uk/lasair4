c597a8be5941a38a9ea73bc72b7b7c149dc68c06
Dave <d.r.young@qub.ac.uk>
2023-03-09 09:38:36 +0000

    schema lookup fixes

--- webserver/lasair/apps/db_schema/utils.py
+++ webserver/lasair/apps/db_schema/utils.py
@@ -81,9 +81,11 @@ def get_schema_for_query_selected(
             listName = []
             if len(select) == 2 and select[0].lower() in [k.lower() for k in schemas.keys()]:
                 if select[1] == "*":
-                    for k, v in schemas[select[0]].items():
-                        tableSchema[k] = v
+                    if select[0] in schemas.keys():
+                        for k, v in schemas[select[0]].items():
+                            tableSchema[k] = v
                 else:
-                    tableSchema[select[1]] = schemas[select[0]][select[1]]
+                    if select[0] in schemas.keys() and select[1] in schemas[select[0]].keys():
+                        tableSchema[select[1]] = schemas[select[0]][select[1]]
 
     return tableSchema
