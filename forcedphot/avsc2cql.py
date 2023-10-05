5d6a716de57392f307c5ed43ac8c00a3495c2458
Roy Williams <roy@roe.ac.uk>
2023-10-05 14:10:10 +0000

    forced phot code and cql

---
+++ forcedphot/avsc2cql.py
@@ -0,0 +1,20 @@
+# file comes from 
+# https://github.com/ZwickyTransientFacility/ztf-avro-alert/blob/master/schema/fp_hist.avsc
+import json
+fp = json.loads(open('fp_hist.avsc').read())
+
+print('CREATE TABLE IF NOT EXISTS forcedphot(')
+
+# objectId is not in schema, will come from the parent alert
+print('  objectid ascii,')
+
+for a in fp['fields']:
+    t = a['type']
+
+    if type(t) is list: t = t[1]
+    if t == 'string':   t = 'ascii'
+    if t == 'long':     t = 'bigint'
+
+    print('  %s %s,' % (a['name'], t))
+print('  PRIMARY KEY (objectid)')
+print(')')
