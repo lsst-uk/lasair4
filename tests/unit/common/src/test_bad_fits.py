b5101e4aac104b11addf4213fb1544f64a238676
Roy Williams <roy@roe.ac.uk>
2023-11-17 13:38:58 +0000

    update

--- tests/unit/common/src/test_bad_fits.py
+++ tests/unit/common/src/test_bad_fits.py
@@ -1,23 +1,23 @@
 import context
 import os, sys, json
 import unittest
-sys.path.append('../../common/src')
+#sys.path.append('../../common/src')
 import bad_fits
 
 class CommonBadFitsTest(unittest.TestCase):
-    def test_bad_fits(self):
-        dir = 'bad_fits_test_files'
-
+    def test_good_moc(self):
         filename = 'bad_fits_test_files/orion.fits'
         message = bad_fits.bad_moc_file(filename)
 #        print(filename, message)
         self.assertIsNone(message)
 
+    def test_bad_mocorder(self):
         filename = 'bad_fits_test_files/orion29.fits'
         message = bad_fits.bad_moc_file(filename)
 #        print(filename, message)
         self.assertIn('Bad MOCORDER', message)
         
+    def test_not_fits(self):
         filename = 'bad_fits_test_files/gbpusd.png'
         message = bad_fits.bad_moc_file(filename)
 #        print(filename, message)
