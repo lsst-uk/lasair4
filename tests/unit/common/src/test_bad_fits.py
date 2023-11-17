c0f77ddf26b6ee76f2b96f4403531c7203a7526d
Roy Williams <roy@roe.ac.uk>
2023-11-17 13:22:05 +0000

    update

--- tests/unit/common/src/test_bad_fits.py
+++ tests/unit/common/src/test_bad_fits.py
@@ -7,9 +7,21 @@ import bad_fits
 class CommonBadFitsTest(unittest.TestCase):
     def test_bad_fits(self):
         dir = 'bad_fits_test_files'
-        for filename in os.listdir(dir):
-            filename = dir +'/'+ filename
-            print(filename, bad_fits.bad_moc_file(filename))
+
+        filename = 'bad_fits_test_files/orion.fits'
+        message = bad_fits.bad_moc_file(filename)
+#        print(filename, message)
+        self.assertIsNone(message)
+
+        filename = 'bad_fits_test_files/orion29.fits'
+        message = bad_fits.bad_moc_file(filename)
+#        print(filename, message)
+        self.assertIn('Bad MOCORDER', message)
+        
+        filename = 'bad_fits_test_files/gbpusd.png'
+        message = bad_fits.bad_moc_file(filename)
+#        print(filename, message)
+        self.assertIn('Cannot open as a FITS file', message)
 
 if __name__ == '__main__':
     import xmlrunner
