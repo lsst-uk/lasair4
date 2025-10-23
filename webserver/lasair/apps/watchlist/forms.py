2606a12d347d2c21c7880e9ba9ab46a6a1c0b935
Roy Williams <roy@roe.ac.uk>
2025-10-23 08:24:15 +0000

    allow empty watchlist

--- webserver/lasair/apps/watchlist/forms.py
+++ webserver/lasair/apps/watchlist/forms.py
@@ -32,9 +32,9 @@ class WatchlistForm(forms.ModelForm):
         cleaned_data = super(WatchlistForm, self).clean()
         conetext = cleaned_data.get("cones_textarea")
         conefile = cleaned_data.get("cones_file")
-        if not conetext and not conefile:
-            msg = "Please either paste your catalogue contents or upload a catalogue file."
-            self.add_error('cones_textarea', msg)
+#        if not conetext and not conefile:
+#            msg = "Please either paste your catalogue contents or upload a catalogue file."
+#            self.add_error('cones_textarea', msg)
 
         if conetext:
             cone_list = []
